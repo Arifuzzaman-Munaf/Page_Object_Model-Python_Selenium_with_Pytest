f04be279324442544f564b4735fa6e085a69223a
Arifuzzaman Munaf <arifuzzamanmunaf@gmail.com>
2021-08-13 21:06:11 +0600

    set up all confihuration

---
+++ Facebook_automation/Config/config.py
@@ -0,0 +1,10 @@
+
+class TestData:
+    # all the webdriver we need to open
+    Chrome_executable_path = "F:\\GitHub\\WebDrivers\\chromedriver"
+    FireFox_executable_path = "F:\\GitHub\\WebDrivers\\geckodriver"
+
+    # information we need to pass through automation
+    Base_Url = "https://www.facebook.com/"
+    email = "arif.qups@gmail.com"
+    password = 'selenium1234'
